9ea2cd39886366896fe0a443c97fb48762234f92
GabbrM <115942603+GabbrM@users.noreply.github.com>
2023-11-12 13:31:24 -0500

    feature: DDD implemented

--- README.md
+++
@@ -1,2 +0,0 @@
-# Web-Services
-Web Services de AKIRA
--- src/main/java/com/upc/backend/akira/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.upc.backend.akira.service;
-
-import com.upc.backend.akira.model.User;
-
-public interface UserService {
-    public abstract User createUser(User user);
-    public abstract User updateUser(User user);
-    public abstract void deleteUserById(Long user);
-}
